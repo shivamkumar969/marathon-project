8650432421ffe17dc86e370d0a454c9e6bb23d72
shivamkumar969 <shivamkumar101202@gmail.com>
2026-04-28 07:28:07 +0530

    Fix: Added missing Link import in ParticipantDashboard

--- smart-event/src/dashboard/participant/ParticipantDashboard.jsx
+++ smart-event/src/dashboard/participant/ParticipantDashboard.jsx
@@ -1,4 +1,4 @@
-import { useLocation } from "react-router-dom";
+import { useLocation, Link } from "react-router-dom";
 import { useState, useEffect } from "react";
 import axios from "axios";
 import config from "../../config";
